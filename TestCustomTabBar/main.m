412ece56a2e97d4421a4a56003867124512ed064
minileixu <763279002@qq.com>
2016-03-25 13:29:04 +0800

    Initial Commit

---
+++ TestCustomTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestCustomTabBar
+//
+//  Created by silver on 16/3/25.
+//  Copyright © 2016年 LeiXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
